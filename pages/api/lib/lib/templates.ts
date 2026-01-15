7fc6e78c56606d19638e4120569a16cfa5e0ae9b
Aawan3967-oss <asifawan3967@gmail.com>
2026-01-15 12:46:31 +0500

    Delete pages/api/lib/lib/templates.ts

--- pages/api/lib/lib/templates.ts
+++
@@ -1,4 +0,0 @@
-export const baseTemplate = {
-  html: `<!DOCTYPE html><html><head><meta charset="UTF-8"></head><body><div id="root"></div></body></html>`,
-  css: `body { font-family: sans-serif; margin: 0; padding: 0; }`
-};
