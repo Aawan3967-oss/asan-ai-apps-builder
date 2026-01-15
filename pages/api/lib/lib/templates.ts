cfcb43d3c18dccf1d66c68862861b4b73af6c18f
Aawan3967-oss <asifawan3967@gmail.com>
2026-01-15 12:42:13 +0500

    Add base HTML and CSS template

---
+++ pages/api/lib/lib/templates.ts
@@ -0,0 +1,4 @@
+export const baseTemplate = {
+  html: `<!DOCTYPE html><html><head><meta charset="UTF-8"></head><body><div id="root"></div></body></html>`,
+  css: `body { font-family: sans-serif; margin: 0; padding: 0; }`
+};
