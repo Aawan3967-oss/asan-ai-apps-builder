c54e95c393df6f90feb4f26bd0e54ce5254acfc5
Aawan3967-oss <asifawan3967@gmail.com>
2026-01-19 13:02:48 +0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import type { AppProps } from 'next/app';
 import { SessionProvider } from "next-auth/react";
-// اگر یہ لائن ایرر دے تو اسے مٹا دیں
-// import '../styles/globals.css'; 
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <div className="min-h-screen bg-black">
+        <Component {...pageProps} />
+      </div>
     </SessionProvider>
   );
 }
