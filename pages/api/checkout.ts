ff1e4a3c08ca7f354b7d3746440f8396fb6d36b7
Aawan3967-oss <asifawan3967@gmail.com>
2026-01-20 13:28:39 +0500

    Delete pages/api/checkout.ts

--- pages/api/checkout.ts
+++
@@ -1,5 +0,0 @@
-export default async function handler(req: any, res: any) {
-  // یہاں آپ Lemon Squeezy یا Stripe کا API لنک استعمال کریں گے
-  const checkoutUrl = "آپ_کا_پیمنٹ_لنک_یہاں"; 
-  res.status(200).json({ url: checkoutUrl });
-}
