6dd1d305ffc7e28f0686049111c75da046cc665b
Aawan3967-oss <asifawan3967@gmail.com>
2026-01-24 02:50:38 +0500

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,9 +4,11 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      // اگر ورسل میں کی موجود نہیں ہوگی تو یہ خالی سٹرنگ استعمال کرے گا
+      clientId: process.env.GOOGLE_CLIENT_ID || "", 
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET, // یہ کی لازمی ہونی چاہیے
+  debug: true, // اس سے لاگز میں زیادہ تفصیل نظر آئے گی
 });
